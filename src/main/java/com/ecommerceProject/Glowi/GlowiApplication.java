7031954578e63ab229910bbb1c341de073f04d56
feriani96 <ferianiyasmine3@gmail.com>
2024-10-03 22:07:46 +0200

    Initialize project with MongoDB configuration

---
+++ src/main/java/com/ecommerceProject/Glowi/GlowiApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerceProject.Glowi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlowiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GlowiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ecommerceProject/Glowi/GlowiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerceProject.Glowi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlowiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
