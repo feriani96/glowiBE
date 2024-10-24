65273b660310686a8a1086df0fefe4b392769888
feriani96 <ferianiyasmine3@gmail.com>
2024-10-24 14:57:44 +0200

    feat: Change Status API

--- src/main/java/com/ecommerceProject/Glowi/controller/admin/AdminOrderController.java
+++ src/main/java/com/ecommerceProject/Glowi/controller/admin/AdminOrderController.java
@@ -3,8 +3,10 @@ package com.ecommerceProject.Glowi.controller.admin;
 import com.ecommerceProject.Glowi.dto.OrderDto;
 import com.ecommerceProject.Glowi.services.admin.adminOrder.AdminOrderService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,5 +22,13 @@ public class AdminOrderController {
     @GetMapping("/placedOrders")
     public ResponseEntity<List<OrderDto>> getAllPlacedOrders(){
         return ResponseEntity.ok(adminOrderService.getAllPlacedOrders());
+    }
+
+    @GetMapping("/order/{orderId}/{status}")
+    public ResponseEntity<?> changeOrderStatus(@PathVariable String orderId, @PathVariable String status){
+        OrderDto orderDto = adminOrderService.changeOrderStatus(orderId, status);
+        if (orderDto == null)
+            return new ResponseEntity<>("Something went wrong!", HttpStatus.BAD_REQUEST);
+        return ResponseEntity.status(HttpStatus.OK).body(orderDto);
     }
 }
